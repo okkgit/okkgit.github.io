3f75c4301c5288f64e457799072f462b9dc4d837
enchao_wang <enchao_wang@qq.com>
2023-03-24 14:54:50 +0800

    go建立调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,7 +62,8 @@ module.exports = {
                     { text: '公司项目', link: "/story/公司项目" },
                 ]
             },
-            { text: '我的简历', link: "/cv/嵌入式" }
+            { text: '嵌入式简历', link: "/cv/嵌入式" },
+            { text: 'Golang简历', link: "/cv/" }
         ],
         displayAllHeaders: true,
         // 侧边栏配置
