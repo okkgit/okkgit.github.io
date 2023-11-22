968fa3e96fef03839663fa0d051ae607568da2dd
okkgit <138770370+okkgit@users.noreply.github.com>
2023-11-22 14:24:43 +0800

    change video card

--- docs/.vuepress/components/VideoCard.vue
+++ docs/.vuepress/components/VideoCard.vue
@@ -1,72 +1,81 @@
 <template>
-    <div class="video-card">
-      <div class="video-wrapper">
-        <iframe :src="videoSrc" frameborder="0" allowfullscreen></iframe>
-      </div>
-      <div class="video-info">
+  <div class="video-card">
+    <div class="video-wrapper" :style="{ width: videoWidth, height: videoHeight }">
+      <iframe :src="videoSrc" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="video-info">
+      <div>
         <h2>{{ videoName }}</h2>
         <p>{{ videoDescription }}</p>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      videoSrc: {
-        type: String,
-        required: true
-      },
-      videoName: {
-        type: String,
-        required: true
-      },
-      videoDescription: {
-        type: String,
-        required: true
-      },
-      width: {
-        type: String,
-        default: "100%" // 默认宽度为100%
-      },
-      height: {
-        type: String,
-        default: "400px" // 默认高度为400px
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    videoSrc: {
+      type: String,
+      required: true
+    },
+    videoName: {
+      type: String,
+      required: true
+    },
+    videoDescription: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: "100%" // 默认宽度为100%
+    },
+    height: {
+      type: String,
+      default: "400px" // 默认高度为400px
     }
-  };
-  </script>
-  
-  <style scoped>
-  .video-card {
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    overflow: hidden;
-    margin: 10px;
+  },
+  computed: {
+    videoWidth() {
+      return this.width;
+    },
+    videoHeight() {
+      return this.height;
+    }
   }
-  
-  .video-wrapper {
-    position: relative;
-    width: 100%;
-    height: 0;
-    padding-bottom: 56.25%; /* 16:9 aspect ratio for responsive iframe */
-  }
-  
-  iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
-  
-  .video-info {
-    padding: 16px;
-  }
-  
-  h2 {
-    margin-bottom: 8px;
-  }
-  
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.video-card {
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 10px;
+  display: flex;
+}
+
+.video-wrapper {
+  flex: 1;
+  overflow: hidden;
+  position: relative;
+}
+
+iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.video-info {
+  flex: 1;
+  padding: 16px;
+}
+
+h2 {
+  margin-bottom: 8px;
+}
+</style>
